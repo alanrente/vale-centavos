cf93c72865e99396c7936b2b102130c28cfbb1ea
Alan Rente <alan.rente@gmail.com>
2024-09-07 01:20:29 -0300

    feat: criacao componente categoria

---
+++ src/hooks/usePngs.tsx
@@ -0,0 +1,24 @@
+export function usePngs() {
+  return {
+    Barraca: "../icons/png/Barraca.png",
+    Bebidas: "../icons/png/Bebidas.png",
+    Bolo: "../icons/png/Bolo.png",
+    Cachorro: "../icons/png/Cachorro.png",
+    Carro: "../icons/png/Carro.png",
+    Coxinhas: "../icons/png/Coxinhas.png",
+    Geladeira: "../icons/png/Geladeira.png",
+    Hamburguer: "../icons/png/Hamburguer.png",
+    HomemBarra: "../icons/png/HomemBarra.png",
+    Joystick: "../icons/png/Joystick.png",
+    Livros: "../icons/png/Livros.png",
+    Mobilha: "../icons/png/Mobilha.png",
+    Notebook: "../icons/png/Notebook.png",
+    Parede: "../icons/png/Parede.png",
+    Pizza: "../icons/png/Pizza.png",
+    Roupas: "../icons/png/Roupas.png",
+    Sorvete: "../icons/png/Sorvete.png",
+    Tenis: "../icons/png/Tenis.png",
+    Tudo: "../icons/png/Tudo.png",
+    Violao: "../icons/png/Violao.png",
+  };
+}
--- src/icons/svg/Carro.tsx
+++
@@ -1,5 +0,0 @@
-import carro from "./Carro_.png";
-
-export function Carro() {
-  return <img src={carro} alt="carro" style={{ width: 36, height: 36 }} />;
-}
